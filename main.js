14119fb96a4eb77e3c837129bd88383091e9faa0
fã do blue pain <134099664+oreo1232@users.noreply.github.com>
2023-08-18 17:45:00 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const wrapper= document. querySelector(".wrapper"),
+qrInput = wrapper. querySelector(". form input"),
+generateBtn = wrapper. querySelector(". form input"),
+qrImg = wrapper. querySelector(".form button"),
+qrImg = wrapper. querySelector(".qr-code img");
+let preValue;
+
+generateBtn.addEventListener("click", () =>)
